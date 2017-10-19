0f797c60aab9a60195cb8aadfdcfebefb6f9c050
Juan Calvopina M <juan.calvopina@gmail.com>
2017-10-18 23:56:01 -0300

    Adds spring-boot dependency

---
+++ prototype-api/src/main/java/com/jcalvopinam/api/PrototypeApiApplication.java
@@ -0,0 +1,13 @@
+package com.jcalvopinam.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototypeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrototypeApiApplication.class, args);
+    }
+
+}
